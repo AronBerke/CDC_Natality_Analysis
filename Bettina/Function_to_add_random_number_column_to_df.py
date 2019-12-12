f50adb0cacc470afe821c78424ae797cd40b8608
Bettina Meier <bmeier01@web.de>
2019-12-12 00:53:32 -0500

    NICU

--- Bettina/Function_to_add_random_number_column_to_df.py
+++
@@ -1,9 +0,0 @@
-def add_random_column_to_df (dataframe):
-    import random
-    mylist = []
-    for i in range(0, dataframe.shape[0]):
-        x = random.randint(1,1000)
-        mylist.append(x)
-    dataframe['RANDOM'] = mylist
-
-    return dataframe
